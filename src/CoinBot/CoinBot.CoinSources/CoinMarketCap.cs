2ef88bae21bf2ced8c07fd228933b748851d86ce
Oliver Hopton <oliver@chezhopton.net>
2017-08-05 17:53:17 +0100

    Protect access to the list of coins with a semaphore.

--- src/CoinBot/CoinBot.CoinSources/CoinMarketCap.cs
+++ src/CoinBot/CoinBot.CoinSources/CoinMarketCap.cs
@@ -14,11 +14,12 @@ namespace CoinBot.CoinSources.CoinMarketCap
     {
         public string Name => "CoinMarketCap";
         private Timer _timer;
-        private TimeSpan _tickInterval => TimeSpan.FromSeconds(10);
+        private TimeSpan _tickInterval;
         private readonly ILogger _logger;
         private readonly HttpClient _httpClient;
         private readonly DataContractJsonSerializer _serializer;
         private List<ICoin> _coins;
+        private readonly SemaphoreSlim _semaphore;
 
         public CoinMarketCap(ILogger logger)
         {
@@ -26,16 +27,34 @@ namespace CoinBot.CoinSources.CoinMarketCap
             this._httpClient = new HttpClient();
             this._coins = new List<ICoin>();
             this._serializer = new DataContractJsonSerializer(typeof(List<CoinMarketCapCoin>));
+            this._semaphore = new SemaphoreSlim(1);
+            this._tickInterval = TimeSpan.FromSeconds(10);
         }
 
         public ICoin GetCoinBySymbol(string symbol)
         {
-            return this._coins.FirstOrDefault(c => string.Compare(c.Symbol, symbol, StringComparison.OrdinalIgnoreCase) == 0);
+            this._semaphore.Wait();
+            try
+            {
+                return this._coins.FirstOrDefault(c => string.Compare(c.Symbol, symbol, StringComparison.OrdinalIgnoreCase) == 0);
+            }
+            finally
+            {
+                this._semaphore.Release();
+            }
         }
 
         public ICoin GetCoinByName(string name)
         {
-            return this._coins.FirstOrDefault(c => string.Compare(c.Name, name, StringComparison.OrdinalIgnoreCase) == 0);
+            this._semaphore.Wait();
+            try
+            {
+                return this._coins.FirstOrDefault(c => string.Compare(c.Name, name, StringComparison.OrdinalIgnoreCase) == 0);
+            }
+            finally
+            {
+                this._semaphore.Release();
+            }
         }
 
         private async Task Tick()
@@ -45,7 +64,15 @@ namespace CoinBot.CoinSources.CoinMarketCap
                 Task<Stream> streamTask = _httpClient.GetStreamAsync("https://api.coinmarketcap.com/v1/ticker/?convert=ETH");
                 List<CoinMarketCapCoin> coinMarketCapCoins = _serializer.ReadObject(await streamTask) as List<CoinMarketCapCoin>;
 
-                this._coins = coinMarketCapCoins.Select(c => (ICoin)c).ToList();
+                await this._semaphore.WaitAsync();
+                try
+                {
+                    this._coins = coinMarketCapCoins.Select(c => (ICoin)c).ToList();
+                }
+                finally
+                {
+                    this._semaphore.Release();
+                }
             }
             catch (Exception e)
             {
