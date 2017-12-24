3ab7008bc620b1f65f5a384a054ec1d5e644d99a
Erik van Leeuwen <erikvl87@gmail.com>
2017-12-25 00:11:00 +0100

    Refactored DI and configuration

---
+++ src/CoinBot.CoinSources/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CoinBot.CoinSources.Extensions
+{
+	/// <summary>
+	/// <see cref="CoinBot.CoinSources"/> <see cref="IServiceCollection"/> extension methods.
+	/// </summary>
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		/// Adds coin sources to the <paramref name="services"/>.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection"/>.</param>
+		/// <returns></returns>
+		public static IServiceCollection AddCoinSources(this IServiceCollection services)
+		{
+			return services
+				.AddSingleton<ICoinSource, CoinMarketCap.CoinMarketCap>();
+		}
+	}
+}
---
+++ src/CoinBot/Startup.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using CoinBot.CoinSources;
+using CoinBot.CoinSources.Extensions;
+using CoinBot.Discord;
+using CoinBot.Discord.Extensions;
+using Discord;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Serilog;
+
+namespace CoinBotCore
+{
+	internal sealed class Startup
+	{
+		/// <summary>
+		/// The <see cref="IConfigurationRoot"/>.
+		/// </summary>
+		private readonly IConfigurationRoot _configuration;
+
+		/// <summary>
+		/// Constructs a <see cref="Startup"/>.
+		/// </summary>
+		internal Startup()
+		{
+			// Load the application configuration
+			_configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: true)
+				.AddEnvironmentVariables()
+				.Build();
+
+			// Build the service provider
+			var services = new ServiceCollection();
+			ConfigureServices(services);
+			var provider = services.BuildServiceProvider();
+
+			// Run the application
+			Run(provider).GetAwaiter().GetResult();
+		}
+
+		/// <summary>
+		/// Adds services to the <paramref name="services"/> container.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection"/>.</param>
+		private void ConfigureServices(IServiceCollection services)
+		{
+			Log.Logger = new LoggerConfiguration()
+				.Enrich.FromLogContext()
+				.WriteTo.Console()
+				.CreateLogger();
+
+			services
+				.AddOptions()
+				.AddSingleton(provider =>
+				{
+					// set up an ILogger
+					var loggerFactory = new LoggerFactory().AddSerilog();
+					return loggerFactory.CreateLogger(nameof(CoinBot));
+				})
+				.AddCoinSources()
+				.AddCoinBot(_configuration);
+		}
+
+		/// <summary>
+		/// Starts the <see cref="CoinBot"/>.
+		/// </summary>
+		/// <param name="provider">The <see cref="IServiceProvider"/>.</param>
+		/// <returns></returns>
+		private static async Task Run(IServiceProvider provider)
+		{
+			//set up a task completion source so we can quit on CTRL+C
+			var exitSource = new TaskCompletionSource<bool>();
+			Console.CancelKeyPress += (sender, eventArgs) =>
+			{
+				eventArgs.Cancel = true;
+				exitSource.SetResult(true);
+			};
+
+			var coinSource = provider.GetRequiredService<ICoinSource>();
+			var bot = provider.GetRequiredService<DiscordBot>();
+
+			// Initialize the bot
+			var botConfig = provider.GetRequiredService<IOptions<DiscordBotSettings>>().Value;
+			await bot.LoginAsync(TokenType.Bot, botConfig.Token);
+
+			// Start the bot & coinSource
+			await bot.StartAsync();
+			coinSource.Start();
+
+			// Keep the application alive until the exitSource task is completed.
+			await exitSource.Task;
+
+			// Stop the bot & coinSource
+			await bot.LogoutAsync();
+			coinSource.Stop();
+		}
+	}
+}
