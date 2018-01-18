07324ede675401c0101593a56fff239af3268396
Erik van Leeuwen <erikvl87@gmail.com>
2018-01-18 23:20:38 +0100

    Use the correct Binance API
    This will also fix a price issue because prevClose was incorrectly used.

--- src/CoinBot.Clients/Binance/BinanceClient.cs
+++ src/CoinBot.Clients/Binance/BinanceClient.cs
@@ -25,7 +25,7 @@ namespace CoinBot.Clients.Binance
 		/// <summary>
 		/// The <see cref="Uri"/> of the CoinMarketCap endpoint.
 		/// </summary>
-		private readonly Uri _endpoint = new Uri("https://www.binance.com/exchange/public/", UriKind.Absolute);
+		private readonly Uri _endpoint = new Uri("https://api.binance.com/api/v1/", UriKind.Absolute);
 
 		/// <summary>
 		/// The <see cref="HttpClient"/>.
@@ -66,15 +66,22 @@ namespace CoinBot.Clients.Binance
 		{
 			try
 			{
-				List<BinanceProduct> products = await this.GetProducts();
-				return products.Select(p => new MarketSummaryDto
-				{
-					BaseCurrrency = this._currencyManager.Get(p.BaseAsset),
-					MarketCurrency = this._currencyManager.Get(p.QuoteAsset),
-					Market = "Binance",
-					Volume = p.Volume,
-					Last = p.PrevClose,
-				}).ToList();
+				List<BinanceTickerDto> products = await this.GetProducts();
+				return products.Select(p =>
+					{
+						if (!this.FindCurrencies(p.Symbol, out var baseCur, out var quoteCur))
+							return null;
+
+						return new MarketSummaryDto
+						{
+							BaseCurrrency = baseCur,
+							MarketCurrency = quoteCur,
+							Market = "Binance",
+							Volume = p.Volume,
+							Last = p.LastPrice,
+						};
+					}).Where(p => p != null)
+					.ToList();
 			}
 			catch (Exception e)
 			{
@@ -88,14 +95,41 @@ namespace CoinBot.Clients.Binance
 		/// Get the market summaries.
 		/// </summary>
 		/// <returns></returns>
-		private async Task<List<BinanceProduct>> GetProducts()
+		private async Task<List<BinanceTickerDto>> GetProducts()
 		{
-			using (HttpResponseMessage response = await this._httpClient.GetAsync(new Uri("product", UriKind.Relative)))
+			using (HttpResponseMessage response = await this._httpClient.GetAsync(new Uri("ticker/24hr", UriKind.Relative)))
 			{
 				string json = await response.Content.ReadAsStringAsync();
-				JObject jObject = JObject.Parse(json);
-				List<BinanceProduct> products = JsonConvert.DeserializeObject<List<BinanceProduct>>(jObject["data"].ToString(), this._serializerSettings);
+				List<BinanceTickerDto> products = JsonConvert.DeserializeObject<List<BinanceTickerDto>>(json, this._serializerSettings);
 				return products;
+			}
+		}
+
+		/// <summary>
+		/// Find the <paramref name="baseCur"/> and <paramref name="quoteCur"/> based on the given <paramref name="symbols"/>.
+		/// </summary>
+		/// <param name="symbols">The symbols.</param>
+		/// <param name="baseCur"></param>
+		/// <param name="quoteCur"></param>
+		/// <returns></returns>
+		private bool FindCurrencies(string symbols, out Currency baseCur, out Currency quoteCur)
+		{
+			baseCur = null;
+			quoteCur = null;
+
+			int i = 3;
+			while (true)
+			{
+				if (i > symbols.Length)
+					return false;
+
+				baseCur = this._currencyManager.Get(symbols.Substring(0, i));
+				quoteCur = this._currencyManager.Get(symbols.Substring(i));
+
+				if (baseCur != null && quoteCur != null)
+					return true;
+
+				i++;
 			}
 		}
 	}
---
+++ src/CoinBot.Clients/Binance/BinanceTickerDto.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+
+namespace CoinBot.Clients.Binance
+{
+	internal sealed class BinanceTickerDto
+	{
+		[JsonProperty("symbol")]
+		public string Symbol { get; set; }
+
+		[JsonProperty("priceChange")]
+		public string PriceChange { get; set; }
+
+		[JsonProperty("priceChangePercent")]
+		public string PriceChangePercent { get; set; }
+
+		[JsonProperty("weightedAvgPrice")]
+		public string WeightedAvgPrice { get; set; }
+
+		[JsonProperty("prevClosePrice")]
+		public string PrevClosePrice { get; set; }
+
+		[JsonProperty("lastPrice")]
+		public decimal? LastPrice { get; set; }
+
+		[JsonProperty("lastQty")]
+		public string LastQty { get; set; }
+
+		[JsonProperty("bidPrice")]
+		public string BidPrice { get; set; }
+
+		[JsonProperty("bidQty")]
+		public string BidQty { get; set; }
+
+		[JsonProperty("askPrice")]
+		public string AskPrice { get; set; }
+
+		[JsonProperty("askQty")]
+		public string AskQty { get; set; }
+
+		[JsonProperty("openPrice")]
+		public string OpenPrice { get; set; }
+
+		[JsonProperty("highPrice")]
+		public string HighPrice { get; set; }
+
+		[JsonProperty("lowPrice")]
+		public string LowPrice { get; set; }
+
+		[JsonProperty("volume")]
+		public decimal? Volume { get; set; }
+
+		[JsonProperty("quoteVolume")]
+		public string QuoteVolume { get; set; }
+
+		[JsonProperty("openTime")]
+		public long OpenTime { get; set; }
+
+		[JsonProperty("closeTime")]
+		public long CloseTime { get; set; }
+
+		[JsonProperty("firstId")]
+		public int FirstId { get; set; }
+
+		[JsonProperty("lastId")]
+		public int LastId { get; set; }
+
+		[JsonProperty("count")]
+		public int Count { get; set; }
+	}
+}
