8145de728f5f3bfb197b3725b246207e8cac6f29
Oliver Hopton <oliver@chezhopton.net>
2018-03-02 09:45:24 +0000

    Use alternative source for images, CMC is blocking them.

--- src/CoinBot.Clients/CoinMarketCap/CoinMarketCapCoin.cs
+++ src/CoinBot.Clients/CoinMarketCap/CoinMarketCapCoin.cs
@@ -10,9 +10,9 @@ namespace CoinBot.Clients.CoinMarketCap
 		[JsonProperty("id")]
 		public string Id { get; set; }
 
-		public string ImageUrl => $"https://files.coinmarketcap.com/static/img/coins/64x64/{this.Id}.png";
+		public string ImageUrl => $"https://raw.githubusercontent.com/cjdowner/cryptocurrency-icons/master/128/color/{this.Symbol.ToLower()}.png";
 
-		public string Url => $"https://coinmarketcap.com/currencies/{this.Id}";
+        public string Url => $"https://coinmarketcap.com/currencies/{this.Id}";
 
 		[JsonProperty("name")]
 		public string Name { get; set; }
