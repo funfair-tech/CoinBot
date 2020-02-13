f88a07dff046c7a5359cd943faf2d060d60d9b33
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-02-13 18:13:00 +0000

    Fixed some code analysis issues

--- src/CoinBot.Discord/Commands/MarketsCommands.cs
+++ src/CoinBot.Discord/Commands/MarketsCommands.cs
@@ -13,141 +13,150 @@ using Microsoft.Extensions.Logging;
 
 namespace CoinBot.Discord.Commands
 {
-	public class MarketsCommands : CommandBase
-	{
-		private readonly CurrencyManager _currencyManager;
-		private readonly ILogger _logger;
-		private readonly MarketManager _marketManager;
+    public class MarketsCommands : CommandBase
+    {
+        private readonly CurrencyManager _currencyManager;
+        private readonly ILogger _logger;
+        private readonly MarketManager _marketManager;
 
-		private readonly char[] _separators = { '-', '/', '\\', ',' };
+        private readonly char[] _separators = {'-', '/', '\\', ','};
 
-		public MarketsCommands(CurrencyManager currencyManager, MarketManager marketManager, ILogger logger)
-		{
-		    this._currencyManager = currencyManager ?? throw new ArgumentNullException(nameof(currencyManager));
-		    this._marketManager = marketManager ?? throw new ArgumentNullException(nameof(marketManager));
-		    this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
-		}
+        public MarketsCommands(CurrencyManager currencyManager, MarketManager marketManager, ILogger logger)
+        {
+            this._currencyManager = currencyManager ?? throw new ArgumentNullException(nameof(currencyManager));
+            this._marketManager = marketManager ?? throw new ArgumentNullException(nameof(marketManager));
+            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
 
-		[Command("markets")]
-		[Summary("get price details per market for a coin, e.g. `!markets FUN` or `!markets ETH/FUN`.")]
-		public async Task Markets([Remainder] [Summary("The input as a single coin symbol or a pair")] string input)
-		{
-			using (this.Context.Channel.EnterTypingState())
-				try
-				{
-					if (!this.GetCurrencies(input, out Currency primaryCurrency, out Currency secondaryCurrency))
-					{
-						await this.ReplyAsync($"Oops! I did not understand `{input}`.");
-						return;
-					}
+        [Command("markets")]
+        [Summary("get price details per market for a coin, e.g. `!markets FUN` or `!markets ETH/FUN`.")]
+        public async Task MarketsAsync([Remainder] [Summary("The input as a single coin symbol or a pair")]
+                                       string input)
+        {
+            using (this.Context.Channel.EnterTypingState())
+                try
+                {
+                    if (!this.GetCurrencies(input, out Currency primaryCurrency, out Currency secondaryCurrency))
+                    {
+                        await this.ReplyAsync($"Oops! I did not understand `{input}`.");
 
-					List<MarketSummaryDto> markets = secondaryCurrency == null
-						? this._marketManager.Get(primaryCurrency).ToList()
-						: this._marketManager.GetPair(primaryCurrency, secondaryCurrency).ToList();
+                        return;
+                    }
 
-					if (!markets.Any())
-					{
-						await this.ReplyAsync($"sorry, no market details found for {input}");
-						return;
-					}
+                    List<MarketSummaryDto> markets = secondaryCurrency == null
+                        ? this._marketManager.Get(primaryCurrency)
+                              .ToList()
+                        : this._marketManager.GetPair(primaryCurrency, secondaryCurrency)
+                              .ToList();
 
-					EmbedBuilder builder = new EmbedBuilder();
-					builder.WithTitle(primaryCurrency.GetTitle());
-					CoinMarketCapCoin details = primaryCurrency.Getdetails<CoinMarketCapCoin>();
-				    if (details != null)
-				    {
-				        builder.WithUrl(details.Url);
-				    }
+                    if (!markets.Any())
+                    {
+                        await this.ReplyAsync($"sorry, no market details found for {input}");
 
-				    AddAuthor(builder);
+                        return;
+                    }
 
-				    if (primaryCurrency.ImageUrl != null)
-				    {
-				        builder.WithThumbnailUrl(primaryCurrency.ImageUrl);
-				    }
+                    EmbedBuilder builder = new EmbedBuilder();
+                    builder.WithTitle(primaryCurrency.GetTitle());
+                    CoinMarketCapCoin details = primaryCurrency.Getdetails<CoinMarketCapCoin>();
 
-				    // Group by exchange, and if looking for a pair orderby volume
-					IEnumerable<IGrouping<string, MarketSummaryDto>> grouped = secondaryCurrency != null
-						? markets
-							.GroupBy(m => m.Market)
-							.OrderByDescending(g => g.Sum(m => m.Volume * m.Last))
-						: markets
-							.GroupBy(m => m.Market);
+                    if (details != null)
+                    {
+                        builder.WithUrl(details.Url);
+                    }
 
-					foreach (IGrouping<string, MarketSummaryDto> group in grouped)
-					{
-						string exchangeName = group.Key;
-						const int maxResults = 3;
-						int totalResults = group.Count();
+                    AddAuthor(builder);
 
-						StringBuilder marketDetails = new StringBuilder();
-						if (secondaryCurrency == null && totalResults > maxResults)
-						{
-							int diff = totalResults - maxResults;
-							if (totalResults < 10)
-							{
-								this.WriteMarketSummaries(marketDetails, group.Take(maxResults));
+                    if (primaryCurrency.ImageUrl != null)
+                    {
+                        builder.WithThumbnailUrl(primaryCurrency.ImageUrl);
+                    }
 
-								marketDetails.AppendLine();
-								marketDetails.AppendLine($"Found {diff} more {primaryCurrency.Symbol} market(s) at {exchangeName}:");
-								marketDetails.AppendLine(string.Join(", ", group.Skip(maxResults).Select(m => $"{m.BaseCurrrency.Symbol}/{m.MarketCurrency.Symbol}")));
-							}
-							else
-							{
-								marketDetails.AppendLine($"`Found {diff} more {primaryCurrency.Symbol} market(s) at {exchangeName}.`");
-							}
-						}
-						else
-						{
-							this.WriteMarketSummaries(marketDetails, group);
-						}
+                    // Group by exchange, and if looking for a pair orderby volume
+                    IEnumerable<IGrouping<string, MarketSummaryDto>> grouped = secondaryCurrency != null
+                        ? markets.GroupBy(m => m.Market)
+                                 .OrderByDescending(g => g.Sum(m => m.Volume * m.Last))
+                        : markets.GroupBy(m => m.Market);
 
-						builder.AddField($"{exchangeName}", marketDetails);
-					}
+                    foreach (IGrouping<string, MarketSummaryDto> group in grouped)
+                    {
+                        string exchangeName = group.Key;
+                        const int maxResults = 3;
+                        int totalResults = group.Count();
 
-					DateTime? lastUpdated = markets.Min(m => m.LastUpdated);
-					AddFooter(builder, lastUpdated);
+                        StringBuilder marketDetails = new StringBuilder();
 
-					string operationName = (secondaryCurrency != null)
-						? $"{primaryCurrency.Name}/{secondaryCurrency.Name}"
-						: primaryCurrency.Name;
+                        if (secondaryCurrency == null && totalResults > maxResults)
+                        {
+                            int diff = totalResults - maxResults;
 
-					await this.ReplyAsync($"Markets for `{operationName}`:", false, builder.Build());
-				}
-				catch (Exception e)
-				{
-					this._logger.LogError(0, e, $"Something went wrong while processing the !markets command with input '{input}'");
-					await this.ReplyAsync("oops, something went wrong, sorry!");
-				}
-		}
+                            if (totalResults < 10)
+                            {
+                                WriteMarketSummaries(marketDetails, group.Take(maxResults));
 
-		private void WriteMarketSummaries(StringBuilder builder, IEnumerable<MarketSummaryDto> markets)
-		{
-			builder.Append("```");
-			foreach (MarketSummaryDto market in markets)
-				builder.AppendLine(market.GetSummary());
-			builder.Append("```");
-		}
+                                marketDetails.AppendLine();
+                                marketDetails.AppendLine($"Found {diff} more {primaryCurrency.Symbol} market(s) at {exchangeName}:");
+                                marketDetails.AppendLine(string.Join(", ",
+                                                                     group.Skip(maxResults)
+                                                                          .Select(m => $"{m.BaseCurrrency.Symbol}/{m.MarketCurrency.Symbol}")));
+                            }
+                            else
+                            {
+                                marketDetails.AppendLine($"`Found {diff} more {primaryCurrency.Symbol} market(s) at {exchangeName}.`");
+                            }
+                        }
+                        else
+                        {
+                            WriteMarketSummaries(marketDetails, group);
+                        }
 
-		private bool GetCurrencies(string input, out Currency primaryCurrency, out Currency secondaryCurrency)
-		{
-			primaryCurrency = null;
-			secondaryCurrency = null;
-			int countSeparators = input.Count(c => this._separators.Contains(c));
-			if (countSeparators > 1)
-				return false;
+                        builder.AddField($"{exchangeName}", marketDetails);
+                    }
 
-			if (countSeparators == 0)
-				primaryCurrency = this._currencyManager.Get(input);
-			else
-			{
-				string first = input.Substring(0, input.IndexOfAny(this._separators));
-				string second = input.Substring(input.IndexOfAny(this._separators) + 1);
-				primaryCurrency = this._currencyManager.Get(first);
-				secondaryCurrency = this._currencyManager.Get(second);
-			}
+                    DateTime? lastUpdated = markets.Min(m => m.LastUpdated);
+                    AddFooter(builder, lastUpdated);
 
-			return true;
-		}
-	}
+                    string operationName = (secondaryCurrency != null) ? $"{primaryCurrency.Name}/{secondaryCurrency.Name}" : primaryCurrency.Name;
+
+                    await this.ReplyAsync($"Markets for `{operationName}`:", false, builder.Build());
+                }
+                catch (Exception e)
+                {
+                    this._logger.LogError(0, e, $"Something went wrong while processing the !markets command with input '{input}'");
+                    await this.ReplyAsync("oops, something went wrong, sorry!");
+                }
+        }
+
+        private static void WriteMarketSummaries(StringBuilder builder, IEnumerable<MarketSummaryDto> markets)
+        {
+            builder.Append("```");
+
+            foreach (MarketSummaryDto market in markets)
+            {
+                builder.AppendLine(market.GetSummary());
+            }
+
+            builder.Append("```");
+        }
+
+        private bool GetCurrencies(string input, out Currency primaryCurrency, out Currency secondaryCurrency)
+        {
+            primaryCurrency = null;
+            secondaryCurrency = null;
+            int countSeparators = input.Count(c => this._separators.Contains(c));
+
+            if (countSeparators > 1) return false;
+
+            if (countSeparators == 0) primaryCurrency = this._currencyManager.Get(input);
+            else
+            {
+                string first = input.Substring(0, input.IndexOfAny(this._separators));
+                string second = input.Substring(input.IndexOfAny(this._separators) + 1);
+                primaryCurrency = this._currencyManager.Get(first);
+                secondaryCurrency = this._currencyManager.Get(second);
+            }
+
+            return true;
+        }
+    }
 }
