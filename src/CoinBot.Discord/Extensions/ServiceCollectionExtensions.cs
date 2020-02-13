81d4d564e537b0aafff064143d4da48e18db70f0
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-02-13 18:16:26 +0000

    Fixed some code analysis issues

--- src/CoinBot.Discord/Extensions/ServiceCollectionExtensions.cs
+++ src/CoinBot.Discord/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using CoinBot.Discord.Commands;
+﻿using System.Threading.Tasks;
+using CoinBot.Discord.Commands;
 using Discord;
 using Discord.Commands;
 using Microsoft.Extensions.Configuration;
@@ -6,43 +7,43 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace CoinBot.Discord.Extensions
 {
-	/// <summary>
-	/// <see cref="CoinBot.Discord"/> <see cref="IServiceCollection"/> extension methods.
-	/// </summary>
-	public static class ServiceCollectionExtensions
-	{
-		/// <summary>
-		/// The <see cref="IConfiguration"/> section key of the <see cref="DiscordBotSettings"/>.
-		/// </summary>
-		private const string DiscordBotSettingsSection = "DiscordBot";
+    /// <summary>
+    /// <see cref="CoinBot.Discord"/> <see cref="IServiceCollection"/> extension methods.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// The <see cref="IConfiguration"/> section key of the <see cref="DiscordBotSettings"/>.
+        /// </summary>
+        private const string DiscordBotSettingsSection = "DiscordBot";
 
-		/// <summary>
-		/// Adds the CoinBot to the <paramref name="services"/>.
-		/// </summary>
-		/// <param name="services">The <see cref="IServiceCollection"/>.</param>
-		/// <param name="configuration">The <see cref="IConfiguration"/>.</param>
-		/// <returns></returns>
-		public static IServiceCollection AddCoinBot(this IServiceCollection services, IConfiguration configuration)
-		{
-			return services
-				.Configure<DiscordBotSettings>(configuration.GetSection(DiscordBotSettingsSection))
-				.AddSingleton(ctx =>
-				{
-					CommandService commandService = new CommandService(new CommandServiceConfig
-					{
-						DefaultRunMode = RunMode.Async,
-						LogLevel = LogSeverity.Debug
-					});
+        /// <summary>
+        /// Adds the CoinBot to the <paramref name="services"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <param name="configuration">The <see cref="IConfiguration"/>.</param>
+        /// <returns></returns>
+        public static async Task<IServiceCollection> AddCoinBotAsync(this IServiceCollection services, IConfiguration configuration)
+        {
+            CommandService commands = await AddCommandsAsync();
 
-					// Add the command modules
-					commandService.AddModuleAsync<CoinCommands>();
-					commandService.AddModuleAsync<GlobalCommands>();
-					commandService.AddModuleAsync<HelpCommands>();
-					commandService.AddModuleAsync<PriceCommands>();
-					commandService.AddModuleAsync<MarketsCommands>();
-					return commandService;
-				})
-				.AddSingleton<DiscordBot>();
-		}
-	}
+            return services.Configure<DiscordBotSettings>(configuration.GetSection(DiscordBotSettingsSection))
+                           .AddSingleton(commands)
+                           .AddSingleton<DiscordBot>();
+        }
+
+        private static async Task<CommandService> AddCommandsAsync()
+        {
+            CommandService commandService = new CommandService(new CommandServiceConfig {DefaultRunMode = RunMode.Async, LogLevel = LogSeverity.Debug});
+
+            // Add the command modules
+            await commandService.AddModuleAsync<CoinCommands>();
+            await commandService.AddModuleAsync<GlobalCommands>();
+            await commandService.AddModuleAsync<HelpCommands>();
+            await commandService.AddModuleAsync<PriceCommands>();
+            await commandService.AddModuleAsync<MarketsCommands>();
+
+            return commandService;
+        }
+    }
 }
