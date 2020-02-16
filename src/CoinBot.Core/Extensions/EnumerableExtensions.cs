6acb31bd83d14509edd5fa30b549e87132be14a0
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-02-16 00:39:22 +0000

    Reduced dependencies

---
+++ src/CoinBot.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CoinBot.Core.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<T> RemoveNulls<T>(this IEnumerable<T?>? items)
+            where T : class
+        {
+            if (items != null)
+            {
+                foreach (T? item in items)
+                {
+                    if (item == null)
+                    {
+                        continue;
+                    }
+
+                    yield return item;
+                }
+            }
+        }
+    }
+}
