ddd9057a4d2b3419c28383b717d90d28b68eaa40
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-02-23 00:40:06 +0000

    basic !price fun support

---
+++ src/CoinBot.Discord/Extensions/FunFairWalletCoinExtensions.cs
@@ -0,0 +1,23 @@
+﻿using CoinBot.Clients.FunFair;
+using CoinBot.Core.Extensions;
+
+namespace CoinBot.Discord.Extensions
+{
+    public static class FunFairWalletCoinExtensions
+    {
+        /// <summary>
+        ///     The USD precision to use when formatting <see cref="FunFairWalletCoin" /> prices.
+        /// </summary>
+        private const int USD_PRICE_PRECISION = 7;
+
+        /// <summary>
+        ///     Get the <paramref name="details" /> price summary in USD and BTC.
+        /// </summary>
+        /// <param name="details"></param>
+        /// <returns></returns>
+        public static string GetPriceSummary(this FunFairWalletCoin details)
+        {
+            return $"{details.PriceUsd.AsUsdPrice(USD_PRICE_PRECISION)}";
+        }
+    }
+}
