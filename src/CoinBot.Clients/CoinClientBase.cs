39d600d06ccdd6aa2a160dfa8baa16e4d47c87e3
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-12-01 19:40:56 +0000

    Fixed unused usings

--- src/CoinBot.Clients/CoinClientBase.cs
+++ src/CoinBot.Clients/CoinClientBase.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Polly;
