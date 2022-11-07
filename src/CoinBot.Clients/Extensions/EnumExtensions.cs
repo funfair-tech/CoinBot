a1deb3a665fe7e0f32c6a6463c24dc3d79bb1ef5
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-11-07 15:41:56 +0000

    Not sure why it can't be bothered to use the generated code

--- src/CoinBot.Clients/Extensions/EnumExtensions.cs
+++ src/CoinBot.Clients/Extensions/EnumExtensions.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using Credfeto.Enumeration.Source.Generation.Attributes;
+using NonBlocking;
 
 namespace CoinBot.Clients.Extensions;
 
@@ -8,4 +10,25 @@ namespace CoinBot.Clients.Extensions;
 [SuppressMessage(category: "ReSharper", checkId: "PartialTypeWithSinglePart", Justification = "Needed for generated code")]
 public static partial class EnumExtensions
 {
+    private static readonly ConcurrentDictionary<Enum, string> CachedNames = new();
+
+    public static string GetName<T>(this T member)
+        where T : Enum
+    {
+        if (CachedNames.TryGetValue(key: member, out string? name))
+        {
+            return name;
+        }
+
+        return CachedNames.GetOrAdd(key: member, ExtractMemberName(member));
+    }
+
+    [SuppressMessage(category: "ToStringWithoutOverrideAnalyzer",
+                     checkId: "ExplicitToStringWithoutOverrideAnalyzer: Calling ToString() on object of type 'T' but it does not override ToString()",
+                     Justification = "OK as T is an enum")]
+    private static string ExtractMemberName<T>(T member)
+        where T : Enum
+    {
+        return Enum.GetName(typeof(T), value: member) ?? member.ToString();
+    }
 }
